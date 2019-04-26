be74ce81361222e14490b553fd8b162a46f983f1
Christoph Berg <myon@debian.org>
2019-04-26 16:39:55 +0200

    Add QR code with QSO data

--- qsl/qsl.py
+++ qsl/qsl.py
@@ -20,7 +20,9 @@ def qsl(c, call):
     cur.execute("""SELECT
             regexp_replace(call, '0', 'Ø', 'g') AS call,
             regexp_replace(date_trunc('minute', start::timestamp)::text, ':00$', '') AS start,
-            round(qrg, 3) || ' MHz' AS qrg,
+            start::date AS qso_date,
+            regexp_replace(date_trunc('minute', start::time)::text, ':00$', '') AS time_on,
+            round(qrg, 3) AS freq,
             mode,
             rsttx, rstrx,
             mycall,
@@ -28,27 +30,33 @@ def qsl(c, call):
             CASE WHEN qslrx = 'J' THEN 'QSL rcvd, tnx!'
                  WHEN qslrx IN ('N', 'W') THEN 'Pse QSL'
                  ELSE qsltx || ' ' || qslrx
-            END AS qsl
+            END AS qsl,
+            CASE WHEN qslrx = 'J' THEN 'TNX' ELSE 'PSE' END AS qsl_rcvd
             FROM log WHERE call = %s
             ORDER BY start, call""", (call,))
 
     qsos = [['Confirming our QSO\nDate',
-             'Frequency',
-             'Mode',
+             'Freq', 'Mode',
              'RST\nsent', 'RST\nrcvd',
              'My Trx, Power, Ant',
              'QSL',
             ]]
+    adif = "OPERATOR;QSO_DATE;TIME_ON;FREQ;MODE;RST_SENT;QSL_RCVD;"
 
     call_formatted = ''
     for qso in cur.fetchall():
         mycall = qso['mycall']
         call_formatted = qso['call']
-        qsos.append([qso['start'], qso['qrg'], qso['mode'],
+        qsos.append([qso['start'], qso['freq'], qso['mode'],
                      qso['rsttx'], qso['rstrx'],
                      qso['mystn'],
                      qso['qsl'],
                     ])
+        adif += "\n%s;%s;%s;%s;%s;%s;%s;" % (qso['mycall'],
+                qso['qso_date'], qso['time_on'],
+                qso['freq'], qso['mode'],
+                qso['rsttx'],
+                qso['qsl_rcvd'])
 
     t = Table(qsos)
     t.setStyle(TableStyle([
@@ -56,20 +64,28 @@ def qsl(c, call):
         ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
         ('RIGHTPADDING', (0, 0), (-1, -1), 0),
         ('SIZE', (0, 0), (-1, 0), 6),
-        ('SIZE', (0, 1), (-1, -1), 9),
+        ('SIZE', (0, 1), (-1, -1), 8),
       ]))
 
-    w,h = t.wrap(100*mm, 30*mm)
-    t.drawOn(c, 0*mm, 10*mm)
+    w, h = t.wrap(100*mm, 30*mm)
+    t.drawOn(c, 0*mm, 29*mm - h)
+
+    qrsize = 28*mm
+    qrw = QrCodeWidget(adif, barBorder=0, barLevel='L')
+    b = qrw.getBounds()
+    (w, h) = (b[2] - b[0], b[3] - b[1])
+    d = Drawing(qrsize, qrsize, transform=[qrsize/w,0,0,qrsize/h,0,0])
+    d.add(qrw)
+    renderPDF.draw(d, c, qslwidth - qslmargin - qrsize, qslmargin)
 
     # picture
     height = 60*mm
     width = height * 768/996 # keep aspect ratio
-    c.drawImage('traarer_muehle.png', 10*mm, 28*mm, width=width, height=height)
+    c.drawImage('traarer_muehle.png', 10*mm, 29*mm, width=width, height=height)
     c.saveState()
     c.setFont("Helvetica", 5)
     c.rotate(90)
-    c.drawString(27*mm, -3*mm, "Picture (C) Julia Berg")
+    c.drawString(30*mm, -3.5*mm, "Picture (c) Julia Berg")
     c.restoreState()
 
     # recipient
@@ -89,24 +105,24 @@ def qsl(c, call):
     renderPDF.draw(d, c, qslwidth - qslmargin - qrsize, qslheight - qslmargin - qrsize)
 
     # myself
-    c.setFont("Helvetica", 40)
-    c.drawString(69*mm, 63*mm, mycall)
+    c.setFont("Helvetica", 33)
+    c.drawString(69.5*mm, 65*mm, mycall)
 
     c.setFont("Helvetica", 14)
-    c.drawString(70*mm, 57*mm, "Christoph Berg")
+    c.drawString(70*mm, 60*mm, "Christoph Berg")
 
-    c.setFont("Helvetica", 10)
+    c.setFont("Helvetica", 8)
     text = c.beginText()
-    text.setTextOrigin(70*mm, 51*mm)
+    text.setTextOrigin(70*mm, 55*mm)
     text.textLines("""
         Born in 1977, Radio Amateur since 1994
         Debian and PostgreSQL Developer
-        Open Source practitioner, Bridge player
+        Open Source practitioner, Bridge player, Geocacher
         """)
     c.drawText(text)
 
     text = c.beginText()
-    text.setTextOrigin(70*mm, 37*mm)
+    text.setTextOrigin(70*mm, 44*mm)
     text.textLines("""
         Rather Str. 76a
         47802 Krefeld-Traar
@@ -116,7 +132,7 @@ def qsl(c, call):
     c.drawText(text)
 
     text = c.beginText()
-    text.setTextOrigin(110*mm, 37*mm)
+    text.setTextOrigin(100*mm, 44*mm)
     text.textLines("""
         cb@df7cb.de
         www.df7cb.de
