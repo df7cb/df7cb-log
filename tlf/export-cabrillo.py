302c6d798c6e2972ab9d82d7dd65cfa67f620d42
Christoph Berg <myon@debian.org>
2021-10-03 15:29:12 +0200

    Refactor cabrillo export; DARC Hell Contest

---
+++ tlf/export-cabrillo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# usage: export-cabrillo <contest name> <filename>
+
+import pyqso.cabrillo
+import psycopg2
+import psycopg2.extras
+import sys
+
+conn = psycopg2.connect("service=cb")
+cur = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+
+contest = sys.argv[1]
+filename = sys.argv[2]
+
+cur.execute("""SELECT qrg AS "FREQ",
+        mode AS "MODE",
+        to_char(start, 'YYYYMMDD') AS "QSO_DATE",
+        to_char(start, 'hh24mi') AS "TIME_ON",
+        regexp_replace(rsttx, '^599', '599 ') AS "RST_SENT",
+        regexp_replace(rstrx, '^599', '599 ') AS "RST_RCVD",
+        call AS "CALL"
+        FROM log
+        WHERE start > now() - '3 days'::interval
+        AND contest ~ %s
+        ORDER BY start, call""",
+        (contest,))
+
+# {'CALL': 'DL3NCI', 'BAND': '80m', 'MODE': 'CW', 'QSO_DATE': '20021226', 'TIME_ON': '1050', 'RST_SENT': '', 'DARC_DOK': 'B25'}
+
+records = cur.fetchall()
+cabrillo = pyqso.cabrillo.Cabrillo()
+cabrillo.write(records,
+        filename,
+        contest=contest,
+        mycall='DF7CB')
