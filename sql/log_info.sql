235c4d5444ffc19330406afa8f0d2750a8ed7a5c
Christoph Berg <myon@debian.org>
2021-10-08 21:45:27 +0200

    Simplify log_info

--- sql/log_info.sql
+++ sql/log_info.sql
@@ -18,37 +18,32 @@ CREATE OR REPLACE FUNCTION dayspan(date1 date, date2 date)
   IMMUTABLE
 AS $$SELECT CASE WHEN date1 <> date2 THEN date1 || '..' || to_char(extract(day from date2), 'FM00') ELSE date1::text END$$;
 
-CREATE OR REPLACE VIEW log_info(info) AS
-  WITH
-  years(years) AS (
-    SELECT jsonb_agg(DISTINCT extract(year from start)) FROM log
-  ),
-  bands(bands) AS (
-    SELECT jsonb_agg(DISTINCT qrg::band) FROM log
-  ),
-  modes(modes) AS (
-    SELECT jsonb_agg(DISTINCT mode) FROM log
-  ),
-  mycalls(mycalls) AS (
-    SELECT jsonb_agg(DISTINCT mycall) FROM log
-  ),
-  contests AS (
-    SELECT week(start), dayspan(min(start::date), max(start::date)), contest
-    FROM log WHERE contest IS NOT NULL
-    GROUP BY 1, 3
-    ORDER BY 1 DESC, 3 DESC
-  ),
-  contestlist(contestlist) AS (
-    SELECT jsonb_agg(jsonb_build_object('week', week, 'dayspan', dayspan, 'contest', contest)) FROM contests
-  )
-  SELECT
-    jsonb_build_object(
-      'years', years,
-      'bands', bands,
-      'modes', modes,
-      'mycalls', mycalls,
-      'contests', contestlist
-    )
-    FROM years, bands, modes, mycalls, contestlist;
+CREATE OR REPLACE VIEW qso_info AS
+SELECT
+  (SELECT jsonb_agg(DISTINCT extract(year from start) ORDER BY extract(year from start) desc)) AS years,
+  (SELECT jsonb_agg(DISTINCT qrg::band)) AS bands,
+  (SELECT jsonb_agg(DISTINCT mode)) AS modes,
+  (SELECT jsonb_agg(DISTINCT mycall)) AS mycalls
+FROM log;
 
-GRANT SELECT ON log_info TO public;
+CREATE OR REPLACE VIEW contest_info(contests) AS
+WITH contest_list AS (
+  SELECT week(start), dayspan(min(start::date), max(start::date)), contest
+  FROM log
+  WHERE contest IS NOT NULL
+  GROUP BY 1, 3
+  ORDER BY 1 DESC, 3 DESC
+)
+SELECT jsonb_agg(jsonb_build_object('week', week, 'dayspan', dayspan, 'contest', contest))
+  FROM contest_list;
+
+CREATE OR REPLACE VIEW log_info(info) AS
+SELECT jsonb_build_object(
+  'years', years,
+  'bands', bands,
+  'modes', modes,
+  'mycalls', mycalls,
+  'contests', contests
+) FROM qso_info, contest_info;
+
+GRANT SELECT ON qso_info, contest_info, log_info TO public;
