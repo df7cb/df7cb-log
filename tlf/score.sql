378022578bcf27bfedf38a15592205cc2e956a63
Christoph Berg <myon@debian.org>
2022-09-26 11:59:49 +0200

    CQWW RTTY

--- tlf/score.sql
+++ tlf/score.sql
@@ -1,5 +1,25 @@
-\set contest 'YO DX HF Contest'
+\set contest 'XYZ Contest'
 
+-- CQWW
+with bands as (
+    select qrg::band as band,
+        count(*) as qso,
+        count(distinct (qrg::band, cty)) as dxcc,
+        count(distinct (qrg::band, regexp_replace(exrx, ' .*', ''))) as cqzone,
+        count(distinct (qrg::band, case when exrx ~ ' ' then exrx end)) as states,
+        sum(case when cty = 'DL' then 1
+                when continent(cty::text) = 'EU' then 2
+                else 3 end) as points
+        from log
+        where contest = 'CQWW RTTY' and start >= current_date - '5 days'::interval
+        group by qrg::band)
+select * from bands
+union all
+select null, sum(qso), sum(dxcc), sum(cqzone), sum(states), sum(points) from bands
+union all
+select null, null, null, null, null, (sum(dxcc) + sum(cqzone) + sum(states)) * sum(points) from bands;
+
+-- YO DX
 with bands as (
     select qrg::band as band,
         count(*) as qso,
