98b5c076dfef9be6d99ea80f12f5c43bee4f2727
Christoph Berg <myon@debian.org>
2022-10-02 20:03:14 +0200

    RSGB International DX Contest

--- tlf/score.sql
+++ tlf/score.sql
@@ -1,4 +1,25 @@
 \set contest 'XYZ Contest'
+\pset null ''
+
+-- generic contest
+with bands as (
+    select qrg::band as "Band",
+        count(*) as "QSO",
+        --sum(case when cty = 'DL' then 1
+        --        when continent(cty::text) = 'EU' then 2
+        --        else 3 end)
+        sum(1)
+          as "QSO-Punkte",
+        count(distinct cty) as "DXCC",
+        array_agg(distinct cty) as "Multis"
+        from log
+        where contest = :'contest' and start >= current_date - '5 days'::interval
+        group by qrg::band)
+select "Band"::text, "QSO", "QSO-Punkte", "DXCC", "Multis" from bands
+union all
+select 'Summe', sum("QSO"), sum("QSO-Punkte"), sum("DXCC"), null from bands
+union all
+select 'Score', null, null, sum("QSO-Punkte") * sum("DXCC"), null from bands;
 
 -- CQWW
 with bands as (
@@ -11,7 +32,7 @@ with bands as (
                 when continent(cty::text) = 'EU' then 2
                 else 3 end) as points
         from log
-        where contest = 'CQWW RTTY' and start >= current_date - '5 days'::interval
+        where contest = :'contest' and start >= current_date - '5 days'::interval
         group by qrg::band)
 select * from bands
 union all
