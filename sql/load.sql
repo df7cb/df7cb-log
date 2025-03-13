fa6e9c7f6cf6b34b7e5c7e42bf6ecde9f4eef893
Christoph Berg <myon@debian.org>
2025-03-14 00:32:33 +0100

    Export sat QSO properly

--- sql/load.sql
+++ sql/load.sql
@@ -6,3 +6,5 @@ COMMIT;
 --\! shp2pgsql -DIs 4326 ne_10m_admin_0_map_subunits/ne_10m_admin_0_map_subunits > ne_10m_admin_0_map_subunits.sql
 \ir ne_10m_admin_0_map_subunits.sql
 \ir map.sql
+
+create function error(message text) returns text as $$begin raise exception '%', message; return null; end;$$ language plpgsql;
